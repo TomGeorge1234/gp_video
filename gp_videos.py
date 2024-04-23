a12761c1608d3f1ae37e1f69b75b6b7bf1042e4c
Tom George <tomgeorge1@btinternet.com>
2024-04-23 22:50:07 +0100

    first

---
+++ gp_videos.py
@@ -0,0 +1,70 @@
+import numpy as np 
+# multiprocessing 
+from multiprocessing import Pool
+from itertools import product
+from functools import partial
+
+N_x = 10
+N_y = 10
+N_t = 10 
+Ls = [0.5,1,2,4,8,16,32]
+Ts = [0.5,1,2,4,8,16,32]
+
+def kernel(x1, x2=None, spatial_length_scale=1, temporal_length_scale=1):
+    """x1 and x2 are arrays of 3D points (x, y, t) of shape (n, 3).ipynb_checkpoints/
+    Returns the kernel matrix of the data points of shape (n, n) where the (i, j)th element is the kernel function evaluated at x1[i] and x2[j]."""
+    if x2 is None:
+        x2 = x1
+    x1_d = x1[:,:2] # N,2 
+    x2_d = x2[:,:2] # N,2
+    x1_t = x1[:,2] # N
+    x2_t = x2[:,2] # N
+    d_dist = np.linalg.norm(x1_d[:,np.newaxis,:] - x2_d[np.newaxis,:,:],axis=2) # N,N
+    t_dist = np.abs(x1_t[:,np.newaxis] - x2_t[np.newaxis,:]) # N,N    
+    exp_d = np.exp(- d_dist**2 / (2*(spatial_length_scale**2))) # N,N
+    exp_t = np.exp(- t_dist**2 / temporal_length_scale) # N,N
+    return exp_d * exp_t
+
+def sample_video(spatial_length_scale, temporal_length_scale, N_x=10, N_y=10, N_t=5):
+    # Get coordinates of 3D space-time points
+    x = np.arange(N_x)
+    y = np.arange(N_y)
+    t = np.arange(N_t)
+    X, Y, T = np.meshgrid(x, y, t)
+    coords = np.array([X,Y,T])
+    coords_shape = (N_x, N_y, N_t)
+    coords = coords.reshape(3,-1) # flatten into 2D
+    
+    # Calculate kernel matrix
+    K = kernel(coords.T, spatial_length_scale=spatial_length_scale, temporal_length_scale=temporal_length_scale)
+    
+    # Sample from N(0,K) using Cholesky decomposition
+    L = np.linalg.cholesky(K + 1e-8*np.eye(K.shape[0]))
+    N = np.random.normal(size=(K.shape[0]))
+    V = np.dot(L, N)
+    V = V.reshape(coords_shape)
+    return V
+
+if __name__ == "__main__":
+
+    # Get inputs from command line 
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--L", type=int, default=1.0)
+    parser.add_argument("--T", type=int, default=1.0)
+    parser.add_argument("--i", type=int, default=10)
+    parser.add_argument("--date", type=str, default=10)
+
+    args = parser.parse_args()
+    L = Ls[args.L]
+    T = Ts[args.T]
+    
+    results = sample_video(
+        spatial_length_scale=L,
+        temporal_length_scale=T,
+        N_x=N_x,
+        N_y=N_y,
+        N_t=N_t
+    )
+    random_hash = str(np.random.randint(1e5))
+    np.savez(f"results/result_{date}/results.npz", results=results, L_idx=args.L, T_idx=args.T, id=args.i)
---
+++ slurm_maker.py
@@ -0,0 +1,45 @@
+#Run this script with --quick handle to get a shorter version of the full script >>> python slurm_maker.py --quick 
+import subprocess
+import numpy as np 
+subprocess.run("rm slurm_script.sh", shell=True)
+import argparse 
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
+#enter sweep ranges here:
+N_I = [0,1,2,3,4]
+N_L = [0,1,2,3,4,5,6]
+N_T = [0,1,2,3,4,5,6]
+
+# get date and tiem in format DDMMYYYY_HHMM using time library
+import time
+date_time = time.strftime("%d%m%Y_%H%M")
+# make a results directory called results_DDMMYYYY_HHMM using os 
+import os
+os.mkdir(f"results/{date_time}")
+
+N_SCRIPTS = len(N_I)*len(N_L)*len(N_T)
+print(f"{N_SCRIPTS} scripts total")
+
+pre_schpeel = [
+"#!/bin/bash \n",
+"#SBATCH --job-name=hpsweep              #name of the job to find when calling >>>sacct or >>>squeue \n",
+"#SBATCH --ntasks=%g                  #how many independent script you are hoping to run \n" %N_SCRIPTS,
+"#SBATCH --time=20:00:00                         #compute time \n",
+"#SBATCH --mem-per-cpu=6000MB \n",
+"#SBATCH --cpus-per-task=1 \n",
+"#SBATCH --output=./logs/%j.log                  #where to save output log files (julia script prints here) \n",
+"#SBATCH --error=./logs/%j.err                   #where to save output error files \n"
+]
+
+
+with open("slurm_script.sh","a") as new: 
+    for line in pre_schpeel:
+        new.write(line)
+
+    for n_i in N_I:
+        for n_l in N_L:
+            for n_t in N_T:
+                new.write(f"srun --ntasks=1 --nodes=1 python gp_videos.py --L {n_l} --T {n_t} --i {n_i} &")
+                new.write("\n")
+    new.write("wait") #stops the script from exiting until all the srun commands have finished

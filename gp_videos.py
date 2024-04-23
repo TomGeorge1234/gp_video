d1b6664be7221d378f3b2f3d3b807e0f711ea5e5
Tom George <tomgeorge1@btinternet.com>
2024-04-23 23:48:06 +0100

    changes

--- gp_videos.py
+++ gp_videos.py
@@ -4,8 +4,8 @@ from multiprocessing import Pool
 from itertools import product
 from functools import partial
 
-N_x = 36
-N_y = 36
+N_x = 20
+N_y = 20
 N_t = 15 
 Ls = [0.5,1,2,4,8,16,32]
 Ts = [0.5,1,2,4,8,16,32]

e3dc4d5bbe91f8063dd0b75a7cd8a69fc30e9f1c
Tom George <tomgeorge1@btinternet.com>
2024-04-24 10:52:19 +0100

    jaxc

--- gp_videos.py
+++ gp_videos.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import jax.numpy as jnp
+import scipy
 # multiprocessing 
 from multiprocessing import Pool
 from itertools import product
@@ -29,6 +30,7 @@ def kernel(x1, x2=None, L=1, T=1):
 
 def sample_video(L, T, N_x=10, N_y=10, N_t=5):
     # Get coordinates of 3D space-time points
+    # assert they are odd numbers
     x = np.arange(N_x)
     y = np.arange(N_y)
     t = np.arange(N_t)
@@ -36,14 +38,29 @@ def sample_video(L, T, N_x=10, N_y=10, N_t=5):
     coords = np.array([X_,Y_,T_])
     coords_shape = (N_x, N_y, N_t)
     coords = coords.reshape(3,-1) # flatten into 2D
+
+    # half coordinates (every other point)
+    xh = np.arange(N_x + (N_x%2==0))[::2]; N_xh = len(xh)
+    yh = np.arange(N_y + (N_y%2==0))[::2]; N_yh = len(yh)
+    th = np.arange(N_t + (N_t%2==0))[::2]; N_th = len(th)
+    Xh_, Yh_, Th_ = np.meshgrid(xh, yh, th)
+    coordsh = np.array([Xh_,Yh_,Th_])
+    coordsh_shape = (N_xh, N_yh, N_th)
+    coordsh = coordsh.reshape(3,-1) # flatten into 2D
+    
     # Calculate kernel matrix
-    K = kernel(coords.T, L=L, T=T)
-    K = jnp.array(K)
+    K = kernel(coordsh.T, L=L, T=T)
+    print(K.shape)
+    # K = np.array(K)
     # Sample from N(0,K) using Cholesky decomposition
-    L = jnp.linalg.cholesky(K + 1e-8*jnp.eye(K.shape[0]))
-    L = np.array(L)
+    L = np.linalg.cholesky(K + 1e-8*np.eye(K.shape[0]))
+    # L = np.array(L)
     N = np.random.normal(size=(K.shape[0]))
     V = np.dot(L, N)
+    V = V.reshape(coordsh_shape)
+
+    # interpolate up to full resolution
+    V = scipy.interpolate.interpn((xh, yh, th), V, coords.T, method='linear', bounds_error=False, fill_value=0)
     V = V.reshape(coords_shape)
     return V
 

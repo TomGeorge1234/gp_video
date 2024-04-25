06dbb7085664f2930c921b35ad47775be8215374
Tom George <tomgeorge1@btinternet.com>
2024-04-25 13:47:33 +0100

    high frequency botch

--- gp_videos.py
+++ gp_videos.py
@@ -4,8 +4,8 @@ import scipy
 N_x = 36 # no. position bins (x)
 N_y = 36 # no. position bins (y)
 N_t = 60 # no. time bins
-Ls = [1,2,4,8,16,32] # decoherence length scale (pixels)
-Ts = [1,2,4,8,16,32] # decoherence time scale (frames)
+Ls = [0.5,1,2,4,8,16,32] # decoherence length scale (pixels)
+Ts = [0.5,1,2,4,8,16,32] # decoherence time scale (frames)
 
 def kernel(x1, x2=None, L=1, T=1):
     """x1 and x2 are arrays of 3D points [x, y, t] of shape (n, 3). 
@@ -23,7 +23,8 @@ def kernel(x1, x2=None, L=1, T=1):
     exp_t = np.exp(- t_dist**2 / (2*(T**2))) # N,N
     return exp_d * exp_t
 
-def sample_video(L, T, N_x=10, N_y=10, N_t=5):
+
+def sample_video(L, T, N_x=36, N_y=36, N_t=60):
     """Samples a video of shape (N_x, N_y, N_t) from a Gaussian process."""
 
     # coordinates of the final video
@@ -31,31 +32,65 @@ def sample_video(L, T, N_x=10, N_y=10, N_t=5):
     y = np.arange(N_y)
     t = np.arange(N_t)
     X_, Y_, T_ = np.meshgrid(x, y, t)
-    coords = np.array([X_,Y_,T_])
-    coords_shape = (N_x, N_y, N_t)
-    coords = coords.reshape(3,-1) # flatten into 2D
 
-    # half coordinates (every other point), well interpolate up to full resolution later
-    xh = np.arange(N_x + (N_x%2==0))[::2]; N_xh = len(xh)
-    yh = np.arange(N_y + (N_y%2==0))[::2]; N_yh = len(yh)
-    th = np.arange(N_t + (N_t%2==0))[::2]; N_th = len(th)
-    Xh_, Yh_, Th_ = np.meshgrid(xh, yh, th)
-    coordsh = np.array([Xh_,Yh_,Th_])
-    coordsh_shape = (N_xh, N_yh, N_th)
-    coordsh = coordsh.reshape(3,-1) # flatten into 2D
+    if (L < 1) or (T < 1):
+        # In this case, pixels / frames are independent and we can sample them separately, lowering the memory cost of sampling the covariance matrix
+        V = np.zeros((N_x, N_y, N_t))  
+        if L < 1: 
+            # sample each pixel independently
+            for px in range(N_x):
+                for py in range(N_y):
+                    x = np.array([px]*N_t)
+                    y = np.array([py]*N_t)
+                    t = np.arange(N_t)
+                    coords = np.array([x,y,t]) # (3,N_t)
+                    K = kernel(coords.T, L=L, T=T)
+                    L_ = np.linalg.cholesky(K + 1e-8*np.eye(K.shape[0]))
+                    N = np.random.normal(size=(K.shape[0])) 
+                    V_ = np.dot(L_, N)
+                    V[px,py,:] = V_    
+        elif T < 1: 
+            for ft in range(N_t):
+                x = np.arange(N_x)
+                y = np.arange(N_y)
+                x_, y_, t = np.meshgrid(x, y, ft)
+                coords = np.array([x_,y_,t])
+                coords_shape = (N_x, N_y)
+                coords = coords.reshape(3,-1)
+                K = kernel(coords.T, L=L, T=T)
+                L_ = np.linalg.cholesky(K + 1e-8*np.eye(K.shape[0]))
+                N = np.random.normal(size=(K.shape[0]))
+                V_ = np.dot(L_, N)
+                V_ = V_.reshape(coords_shape)
+                V[:,:,ft] = V_
+
+    else: 
+        coords = np.array([X_,Y_,T_])
+        coords_shape = (N_x, N_y, N_t)
+        coords = coords.reshape(3,-1) # flatten into 2D
+        # In this case you cant make the independence assumption but you can sample at half resolution and then interpolate.
+        # half coordinates (every other point), well interpolate up to full resolution later
+        xh = np.arange(N_x + (N_x%2==0))[::2]; N_xh = len(xh)
+        yh = np.arange(N_y + (N_y%2==0))[::2]; N_yh = len(yh)
+        th = np.arange(N_t + (N_t%2==0))[::2]; N_th = len(th)
+        Xh_, Yh_, Th_ = np.meshgrid(xh, yh, th)
+        coordsh = np.array([Xh_,Yh_,Th_])
+        coordsh_shape = (N_xh, N_yh, N_th)
+        coordsh = coordsh.reshape(3,-1) # flatten into 2D
+        
+        # Calculate kernel matrix
+        K = kernel(coordsh.T, L=L, T=T)
+
+        # To sample from a Gaussian N(0,K) is very time consuming. It turns out if you can calculte the cholesky decomposition of K = LL^T, then you can sample from N(0,K) by computing N(0,I) and multiplying by L. see https://rinterested.github.io/statistics/multivariate_normal_draws
+        L_ = np.linalg.cholesky(K + 1e-8*np.eye(K.shape[0])) # this is the expensive step, borderline possible for dim(K) < 20,000, after this...good luck! The problem is memory.
+        N = np.random.normal(size=(K.shape[0]))
+        V = np.dot(L_, N)
+        V = V.reshape(coordsh_shape)
+
+        # interpolate up to full resolution
+        V = scipy.interpolate.interpn((xh, yh, th), V, coords.T, method='linear', bounds_error=False, fill_value=0)
+        V = V.reshape(coords_shape)
     
-    # Calculate kernel matrix
-    K = kernel(coordsh.T, L=L, T=T)
-
-    # To sample from a Gaussian N(0,K) is very time consuming. It turns out if you can calculte the cholesky decomposition of K = LL^T, then you can sample from N(0,K) by computing N(0,I) and multiplying by L. see https://rinterested.github.io/statistics/multivariate_normal_draws
-    L = np.linalg.cholesky(K + 1e-8*np.eye(K.shape[0])) # this is the expensive step, borderline possible for dim(K) < 20,000, after this...good luck! The problem is memory.
-    N = np.random.normal(size=(K.shape[0]))
-    V = np.dot(L, N)
-    V = V.reshape(coordsh_shape)
-
-    # interpolate up to full resolution
-    V = scipy.interpolate.interpn((xh, yh, th), V, coords.T, method='linear', bounds_error=False, fill_value=0)
-    V = V.reshape(coords_shape)
     return V
 
 if __name__ == "__main__":
